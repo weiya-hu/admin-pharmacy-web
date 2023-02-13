a40d1e73d117058d0e1df835daa3966b2424ac41
户维娅 <1063201840@qq.com>
2023-02-13 15:38:33 +0800

    feat:label组件

--- src/views/insurance/customer/components/labelExplain.vue
+++ src/views/insurance/customer/components/labelExplain.vue
@@ -8,7 +8,7 @@
       trigger="hover"
       :content="props.explain"
     >
-      <template #default v-if="props.explain">
+      <template #default v-if="!props.explain">
         <slot name="explain"></slot>
       </template>
       <template #reference>
@@ -23,7 +23,7 @@
 /**
  * @name Label
  * @param label:label文字
- * @param explain:悬浮显示的解释文字，单纯的文字用这个
+ * @param explain:悬浮显示的解释文字，单纯的文字用这个,传了这个参数，explain插槽的就不会显示
  * @slot explain:悬浮显示的解释文字，里面有点击的链接的用这个。点击的链接标签统一用<el-link type="primary" href="" target="_blank"></el-link>
  */
 import { QuestionFilled, InfoFilled } from '@element-plus/icons-vue'
