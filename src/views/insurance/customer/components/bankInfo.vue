3af768116221e21a7659cb7d29e22d81577e2f9a
wangmeixi <wangmeixi@shanhaiping.com>
2023-02-14 17:44:23 +0800

    结算银行账户，结算规则

--- src/views/insurance/customer/components/bankInfo.vue
+++ src/views/insurance/customer/components/bankInfo.vue
@@ -1,15 +1,170 @@
 <template>
-  <div>
-    <el-card>
-      结算银行账户
+  <div class="app-container">
+    <el-card class="box-card">
+      <template #header>
+        <div class="card-header">
+          <span style="white-space: nowrap;">结算银行账户</span>
+          <span class="desc"
+          >请填写商家提现收款的银行账户信息。</span
+          >
+        </div>
+      </template>
+      <el-form ref="bankRef" model="form" :rules="rules">
+        <el-form-item prop="bankAccountType">
+          <template #label>
+            <labelExplain label="账户类型">
+              <template #explain>
+                <div>1、若主体为企业/政府机关/事业单位/社会组织，可填写：对公银行账户<br />2、若主体为个体户，可选择填写：对公银行账户或经营者个人银行卡</div>
+              </template>
+            </labelExplain>
+          </template>
+          <el-select
+              v-model="form.bankAccountType"
+              placeholder="请选择账户类型"
+              style="width: 100%;"
+          >
+            <el-option label="对公银行账户" value="BANK_ACCOUNT_TYPE_CORPORATE"/>
+            <el-option label="经营者个人银行卡" value="BANK_ACCOUNT_TYPE_PERSONAL"/>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item prop="accountName">
+          <template #label>
+            <labelExplain label="开户名称">
+              <template #explain>
+                <div>1、选择“经营者个人银行卡”时，开户名称必须与“经营者证件姓名”一致<br />2、选择“对公银行账户”时，开户名称必须与营业执照上的“商户名称”一致</div>
+              </template>
+            </labelExplain>
+          </template>
+          <el-input v-model="form.accountName" placeholder="请输入开户名称" />
+        </el-form-item>
+
+        <el-form-item prop="accountBank">
+          <template #label>
+            <labelExplain label="开户银行">
+              <template #explain>
+                <div>1、17家直连银行，请根据开户银行对照表直接填写银行名<br />2、非17家直连银行，该参数请填写为“其他银行”。</div>
+              </template>
+            </labelExplain>
+          </template>
+          <el-select v-model="form.accountBank" placeholder="请选择开户银行" style="width: 100%;">
+            <el-option v-for="item in accountBankList" :key="item.value" :label="item.label" :value="item.value" />
+          </el-select>
+        </el-form-item>
+
+        <el-form-item prop="bankAddressCode">
+          <template #label>
+            <labelExplain label="开户银行省市编码">
+              <template #explain>
+                <div>至少精确到市</div>
+              </template>
+            </labelExplain>
+          </template>
+          <el-input v-model="form.bankAddressCode" placeholder="请输入开户银行省市编码" />
+        </el-form-item>
+
+        <el-form-item prop="bankBranchId">
+          <template #label>
+            <labelExplain label="开户银行联行号">
+              <template #explain>
+                <div>1、17家直连银行无需填写，如为其他银行，则开户银行全称（含支行）和开户银行联行号二选一。</div>
+              </template>
+            </labelExplain>
+          </template>
+          <el-input v-model="form.bankBranchId" placeholder="请输入银行联行号" />
+        </el-form-item>
+
+        <el-form-item prop="bankName">
+          <template #label>
+            <labelExplain label="开户银行全称（含支行）">
+              <template #explain>
+                <div>1、17家直连银行无需填写，如为其他银行，则开户银行全称（含支行）和 开户银行联行号二选一<br />2、需填写银行全称，如"深圳农村商业银行XXX支行"，</div>
+              </template>
+            </labelExplain>
+          </template>
+          <el-input v-model="form.bankName" placeholder="请输入开户银行全称（含支行）" />
+        </el-form-item>
+
+        <el-form-item prop="accountNumber">
+          <template #label>
+            <labelExplain label="银行账号">
+              <template #explain>
+                <div>数字，长度遵循系统支持的卡号长度要求</div>
+              </template>
+            </labelExplain>
+          </template>
+          <el-input v-model="form.accountNumber" placeholder="请输入银行账号" />
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
 
 <script setup>
+import {reactive, ref} from "vue";
+import LabelExplain from "@/views/insurance/customer/components/labelExplain";
 
+const form = ref({
+  bankAccountType: '',//账户类型
+  accountName: '', //开户名称
+  accountBank: '', //开户银行
+  bankAddressCode: '', //开户银行省市编码
+  bankBranchId: '', //开户银行联行号
+  bankName: '', //开户银行全称（含支行）
+  accountNumber: '' //银行账号
+})
+
+const rules = reactive({
+  bankAccountType: [{ required: true, message: "请选择账户类型", trigger: "change" }],
+  accountName: [{ required: true, message: "请输入开户名称", trigger: "blur" }],
+  accountBank: [{ required: true, message: "请选择开户银行", trigger: "change" }],
+  bankAddressCode: [{ required: true, message: "请输入开户银行省市编码", trigger: "blur" }],
+  // bankBranchId: [{ required: true, message: "请输入开户银行联行号", trigger: "blur" }],
+  // bankName: [{ required: true, message: "请输入开户银行全称（含支行）", trigger: "blur" }],
+  accountNumber: [{ required: true, message: "请输入银行账号", trigger: "blur" }]
+})
+
+const accountBankList = ref([
+  { value: '1', label: '工商银行' },
+  { value: '2', label: '交通银行' },
+  { value: '3', label: '招商银行' },
+  { value: '4', label: '民生银行' },
+  { value: '5', label: '中信银行' },
+  { value: '6', label: '浦发银行' },
+  { value: '7', label: '兴业银行' },
+  { value: '8', label: '光大银行' },
+  { value: '9', label: '广发银行' },
+  { value: '10', label: '平安银行' },
+  { value: '11', label: '北京银行' },
+  { value: '12', label: '华夏银行' },
+  { value: '13', label: '农业银行' },
+  { value: '14', label: '建设银行' },
+  { value: '15', label: '邮政储蓄银行' },
+  { value: '16', label: '中国银行' },
+  { value: '17', label: '宁波银行' },
+  { value: '18', label: '其他银行' }
+])
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.app-container {
+  .box-card {
+    width: 650px;
 
+    .card-header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      font-size: 24px;
+      font-weight: bold;
+
+      .desc {
+        width: 350px;
+        font-weight: normal;
+        font-size: 12px;
+        margin-left: 40px;
+      }
+    }
+  }
+}
 </style>
--- src/views/insurance/customer/components/settlementInfo.vue
+++ src/views/insurance/customer/components/settlementInfo.vue
@@ -1,13 +1,238 @@
 <template>
   <div class="app-container">
-    <el-card>结算规则</el-card>
+    <el-card class="box-card">
+      <template #header>
+        <div class="card-header">
+          <span style="white-space: nowrap;">结算规则</span>
+          <span class="desc"
+          >请填写商家的结算费率规则、特殊资质等信息。</span
+          >
+        </div>
+      </template>
+      <el-form ref="settlementRef" model="form" :rules="rules">
+        <el-form-item prop="settlementId">
+          <template #label>
+            <labelExplain label="入驻结算规则ID">
+              <template #explain>
+                <div>请选择结算规则ID，详细参见<el-link href="https://kf.qq.com/faq/220228IJb2UV220228uEjU3Q.html" type="primary" target="_blank">费率结算规则对照表</el-link>，示例值：719</div>
+              </template>
+            </labelExplain>
+          </template>
+          <el-input maxlength="3" v-model="form.settlementId" placeholder="请输入入驻结算规则ID" />
+        </el-form-item>
+
+        <el-form-item prop="qualificationType">
+          <template #label>
+            <labelExplain label="所属行业">
+              <template #explain>
+                <div>填写指定行业名称，详细参见<el-link href="https://kf.qq.com/faq/220228IJb2UV220228uEjU3Q.html" type="primary" target="_blank">费率结算规则对照表</el-link>，示例值：餐饮</div>
+              </template>
+            </labelExplain>
+          </template>
+          <el-input maxlength="128" v-model="form.qualificationType" placeholder="请输入所属行业" />
+        </el-form-item>
+
+        <el-form-item prop="qualifications">
+          <template #label>
+            <labelExplain label="特殊资质图片">
+              <template #explain>
+                <div>根据所属行业的特殊资质要求提供，详情查看<el-link href="https://kf.qq.com/faq/220228IJb2UV220228uEjU3Q.html" type="primary" target="_blank">费率结算规则对照表</el-link>，示例值：0P3ng6KTIW4-Q_l2FjmFJBZR9FwczhJehHhAZN6BKXQPcs-VvdSo</div>
+              </template>
+            </labelExplain>
+          </template>
+          <el-upload
+              v-model:file-list="qFileList"
+              :action="uploadData.uploadUrl"
+              :headers="{'Authorization':uploadData.token}"
+              method:="get"
+              accept="image/*"
+              multiple
+              :limit="5"
+              :show-file-list="true"
+              list-type="picture-card"
+              :on-success="qHandleAvatarSuccess"
+              :on-preview="qHandlePictureCardPreview"
+              :before-upload="beforeAvatarUpload"
+          >
+            <el-icon class="form-img" color="#666666"><Plus /></el-icon>
+          </el-upload>
+          <el-dialog v-model="qDialogVisible" >
+            <img :src="qDialogImageUrl" alt="" style="width: 100%;object-fit: contain;" />
+          </el-dialog>
+        </el-form-item>
+
+        <el-form-item prop="activitiesId">
+          <template #label>
+            <labelExplain label="优惠费率活动ID">
+              <template #explain>
+                <div>选择指定活动ID，商户有意向报名优惠费率活动。详细参见<el-link href="https://pay.weixin.qq.com/wiki/doc/apiv3_partner/terms_definition/chapter1_1_3.shtml#part-10l" type="primary" target="_blank">优惠费率活动对照表</el-link>，示例值：20191030111cff5b5e</div>
+              </template>
+            </labelExplain>
+          </template>
+          <el-input maxlength="32" v-model="form.activitiesId" placeholder="请输入优惠费率活动ID" />
+        </el-form-item>
+
+        <el-form-item prop="activitiesRate">
+          <template #label>
+            <labelExplain label="优惠费率活动值">
+              <template #explain>
+                <div>根据优惠费率活动规则，由服务商自定义填写，支持两个小数点，需在优惠费率活动ID指定费率范围内</div>
+              </template>
+            </labelExplain>
+          </template>
+          <el-input maxlength="50" v-model="form.activitiesRate" placeholder="请输入优惠费率活动值" />
+        </el-form-item>
+
+        <el-form-item prop="activitiesAdditions">
+          <template #label>
+            <labelExplain label="优惠费率活动补充材料">
+              <template #explain>
+                <div>根据所选优惠费率活动，提供相关材料，详细参见优惠费率活动对照表<el-link href="https://kf.qq.com/faq/220228IJb2UV220228uEjU3Q.html" type="primary" target="_blank">费率结算规则对照表</el-link>，示例值：餐饮</div>
+              </template>
+            </labelExplain>
+          </template>
+          <el-upload
+              v-model:file-list="addFileList"
+              :action="uploadData.uploadUrl"
+              :headers="{'Authorization':uploadData.token}"
+              method:="get"
+              accept="image/*"
+              multiple
+              :limit="5"
+              :show-file-list="true"
+              list-type="picture-card"
+              :on-success="addHandleAvatarSuccess"
+              :on-error="addHandleAvatarErr"
+              :on-preview="handlePictureCardPreview"
+              :before-upload="beforeAvatarUpload"
+          >
+            <el-icon class="form-img" color="#666666"><Plus /></el-icon>
+          </el-upload>
+          <el-dialog v-model="dialogVisible" >
+            <img :src="dialogImageUrl" alt="" style="width: 100%;object-fit: contain;" />
+          </el-dialog>
+        </el-form-item>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
 <script setup>
+import {reactive, ref} from "vue";
+import LabelExplain from "@/views/insurance/customer/components/labelExplain";
+import {getToken} from "@/utils/auth";
+import {ElMessage} from "element-plus";
+
+const form = ref({
+  settlementId: '', //入驻结算规则ID
+  qualificationType: '', //所属行业
+  qualifications: [], //特殊资质图片
+  activitiesId: '', //优惠费率活动ID
+  activitiesRate: '', //优惠费率活动值
+  activitiesAdditions: [] //优惠费率活动补充材料
+})
+const addFileList = ref([]);
+const qFileList = ref([])
+const dialogVisible = ref(false);
+const dialogImageUrl = ref('');
+const qDialogVisible = ref(false);
+const qDialogImageUrl = ref('');
+
+const uploadData = ref({
+  uploadUrl: import.meta.env.VITE_APP_BASE_API + 'pay/media/wxPictureUpload',
+  token: getToken()
+})
+
+const rules = reactive({
+  settlementId: [{ required: true, message: "请输入入驻结算规则ID", trigger: "blur" }],
+  qualificationType: [{ required: true, message: "请输入所属行业", trigger: "blur" }],
+  qualifications: [{ required: true, message: "请上传特殊资质图片", trigger: "change" }],
+  activitiesId: [{ required: true, message: "请输入优惠费率活动ID", trigger: "blur" }],
+  activitiesRate: [{ required: true, message: "请输入优惠费率活动值", trigger: "blur" }],
+  activitiesAdditions: [{ required: true, message: "请上传优惠费率活动补充材料", trigger: "change" }],
+})
+// 上传验证
+const beforeAvatarUpload = (file) => {
+  const isPicture = file.type === 'image/jpeg' || file.type === 'image/png'
+  const isLt2M = file.size / 1024 / 1024 < 2
+  if (!isPicture) {
+    ElMessage.error('请上传jepg,png,jpg格式的图片!')
+    return false
+  } else if (!isLt2M) {
+    ElMessage.error('上传图片不能超过2M!')
+    return false
+  }
+  return true
+}
+//特殊资质图片
+const qHandleAvatarSuccess = (res) => {
+  if (res.code === 200) {
+    qFileList.value = form.value.qualifications
+  } else {
+    ElMessage.error(res.msg)
+    qFileList.value = []
+  }
+}
+const qHandlePictureCardPreview = (uploadFile) => {
+  qDialogImageUrl.value = uploadFile.url
+  qDialogVisible.value = true
+}
+
+// 优惠费率活动补充材料
+const addHandleAvatarSuccess = (res) => {
+  if (res.code === 200) {
+    addFileList.value = form.value.activitiesAdditions
+  } else {
+    ElMessage.error(res.msg)
+    addFileList.value = []
+  }
+}
+const handlePictureCardPreview = (uploadFile) => {
+  dialogImageUrl.value = uploadFile.url
+  dialogVisible.value = true
+}
 
 </script>
 
 <style lang="scss" scoped>
+.app-container {
+  .box-card {
+    width: 650px;
 
+    .card-header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      font-size: 24px;
+      font-weight: bold;
+
+      .desc {
+        width: 350px;
+        font-weight: normal;
+        font-size: 12px;
+        margin-left: 40px;
+      }
+    }
+  }
+  .form-img {
+    width: 60px;
+    height: 60px;
+    border: 1px solid var(--el-border-color);
+    border-radius: 6px;
+  }
+  .el-form-item {
+    align-items: center;
+    :deep(.el-form-item__label) {
+      font-weight: 700;
+    }
+    :deep(.el-upload--picture-card) {
+      --el-upload-picture-card-size: 60px;
+      border: 0;
+    }
+    :deep(.el-upload-list--picture-card) .el-upload-list__item {
+      width: 60px;
+      height: 60px;
+    }
+  }
+}
 </style>
