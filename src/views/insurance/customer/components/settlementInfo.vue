e13323d6f8a2943ecf7873d128477fdf07a095ca
wangmeixi <wangmeixi@shanhaiping.com>
2023-02-15 19:24:07 +0800

    结算规则

--- src/views/insurance/customer/components/settlementInfo.vue
+++ src/views/insurance/customer/components/settlementInfo.vue
@@ -9,12 +9,12 @@
           >
         </div>
       </template>
-      <el-form ref="settlementRef" model="form" :rules="rules">
+      <el-form ref="settlementRef" :model="form" :rules="rules">
         <el-form-item prop="settlementId">
           <template #label>
             <labelExplain label="入驻结算规则ID">
               <template #explain>
-                <div>请选择结算规则ID，详细参见<el-link href="https://kf.qq.com/faq/220228IJb2UV220228uEjU3Q.html" type="primary" target="_blank">费率结算规则对照表</el-link>，示例值：719</div>
+                <div>请选择结算规则ID，详细参见<el-link type="primary" @click="onClick('settlement')">费率结算规则对照表</el-link>，示例值：719</div>
               </template>
             </labelExplain>
           </template>
@@ -25,47 +25,32 @@
           <template #label>
             <labelExplain label="所属行业">
               <template #explain>
-                <div>填写指定行业名称，详细参见<el-link href="https://kf.qq.com/faq/220228IJb2UV220228uEjU3Q.html" type="primary" target="_blank">费率结算规则对照表</el-link>，示例值：餐饮</div>
+                <div>填写指定行业名称，详细参见<el-link type="primary" @click="onClick('settlement')">费率结算规则对照表</el-link>，示例值：餐饮</div>
               </template>
             </labelExplain>
           </template>
-          <el-input maxlength="128" v-model="form.qualificationType" placeholder="请输入所属行业" />
+          <el-select v-model="form.qualificationType" filterable @change="handleChange" placeholder="请选择所属行业" style="width: 100%;">
+            <el-option v-for="item in typeList" :value="item.id" :label="item.qualificationType" :key="item.id" />
+          </el-select>
+<!--          <el-input maxlength="128" v-model="form.qualificationType" placeholder="请输入所属行业" />-->
         </el-form-item>
 
-        <el-form-item prop="qualifications">
+        <el-form-item prop="qualifications" v-if="otherAsk === 1">
           <template #label>
             <labelExplain label="特殊资质图片">
               <template #explain>
-                <div>根据所属行业的特殊资质要求提供，详情查看<el-link href="https://kf.qq.com/faq/220228IJb2UV220228uEjU3Q.html" type="primary" target="_blank">费率结算规则对照表</el-link>，示例值：0P3ng6KTIW4-Q_l2FjmFJBZR9FwczhJehHhAZN6BKXQPcs-VvdSo</div>
+                <div>1、根据所属行业的特殊资质要求提供，详情查看<el-link type="primary" @click="onClick('settlement')">费率结算规则对照表</el-link><br />2、最多可上传5张照片</div>
               </template>
             </labelExplain>
           </template>
-          <el-upload
-              v-model:file-list="qFileList"
-              :action="uploadData.uploadUrl"
-              :headers="{'Authorization':uploadData.token}"
-              method:="get"
-              accept="image/*"
-              multiple
-              :limit="5"
-              :show-file-list="true"
-              list-type="picture-card"
-              :on-success="qHandleAvatarSuccess"
-              :on-preview="qHandlePictureCardPreview"
-              :before-upload="beforeAvatarUpload"
-          >
-            <el-icon class="form-img" color="#666666"><Plus /></el-icon>
-          </el-upload>
-          <el-dialog v-model="qDialogVisible" >
-            <img :src="qDialogImageUrl" alt="" style="width: 100%;object-fit: contain;" />
-          </el-dialog>
+          <ShpUploadFile v-model="form.qualifications" :limit="5" :multiple="true" flag="qualifications"></ShpUploadFile>
         </el-form-item>
 
         <el-form-item prop="activitiesId">
           <template #label>
             <labelExplain label="优惠费率活动ID">
               <template #explain>
-                <div>选择指定活动ID，商户有意向报名优惠费率活动。详细参见<el-link href="https://pay.weixin.qq.com/wiki/doc/apiv3_partner/terms_definition/chapter1_1_3.shtml#part-10l" type="primary" target="_blank">优惠费率活动对照表</el-link>，示例值：20191030111cff5b5e</div>
+                <div>选择指定活动ID，商户有意向报名优惠费率活动。详细参见<el-link type="primary" @click="onClick('activity')">优惠费率活动对照表</el-link>，示例值：20191030111cff5b5e</div>
               </template>
             </labelExplain>
           </template>
@@ -76,7 +61,7 @@
           <template #label>
             <labelExplain label="优惠费率活动值">
               <template #explain>
-                <div>根据优惠费率活动规则，由服务商自定义填写，支持两个小数点，需在优惠费率活动ID指定费率范围内</div>
+                <div>根据优惠费率活动规则，由服务商自定义填写，支持两个小数点，需在优惠费率活动ID指定费率范围内，示例值：0.6</div>
               </template>
             </labelExplain>
           </template>
@@ -87,42 +72,62 @@
           <template #label>
             <labelExplain label="优惠费率活动补充材料">
               <template #explain>
-                <div>根据所选优惠费率活动，提供相关材料，详细参见优惠费率活动对照表<el-link href="https://kf.qq.com/faq/220228IJb2UV220228uEjU3Q.html" type="primary" target="_blank">费率结算规则对照表</el-link>，示例值：餐饮</div>
+                <div>1、根据所选优惠费率活动，提供相关材料，详细参见<el-link type="primary" @click="onClick('activity')">优惠费率活动对照表</el-link><br />2、最多可上传5张照片</div>
               </template>
             </labelExplain>
           </template>
-          <el-upload
-              v-model:file-list="addFileList"
-              :action="uploadData.uploadUrl"
-              :headers="{'Authorization':uploadData.token}"
-              method:="get"
-              accept="image/*"
-              multiple
-              :limit="5"
-              :show-file-list="true"
-              list-type="picture-card"
-              :on-success="addHandleAvatarSuccess"
-              :on-error="addHandleAvatarErr"
-              :on-preview="handlePictureCardPreview"
-              :before-upload="beforeAvatarUpload"
-          >
-            <el-icon class="form-img" color="#666666"><Plus /></el-icon>
-          </el-upload>
-          <el-dialog v-model="dialogVisible" >
-            <img :src="dialogImageUrl" alt="" style="width: 100%;object-fit: contain;" />
-          </el-dialog>
+          <ShpUploadFile v-model="form.activitiesAdditions" :limit="5" :multiple="true" flag="activitiesAdditions"></ShpUploadFile>
         </el-form-item>
+
+<!--        <el-form-item>-->
+<!--          <el-button @click="submit">保存</el-button>-->
+<!--        </el-form-item>-->
       </el-form>
     </el-card>
+    <!-- 费率结算规则对照表 -->
+    <el-dialog v-model="dialogVisibleTable" title="费率结算规则对照表" width="80%" draggable>
+      <el-table :data="tableData" height="700">
+        <el-table-column prop="settlementId" label="结算规则ID" />
+        <el-table-column prop="subjectType" label="主体类型" />
+        <el-table-column prop="rule" label="结算规则描述" />
+        <el-table-column prop="fitType" label="适用行业范围概述" width="270" />
+        <el-table-column prop="qualificationId" label="行业ID" />
+        <el-table-column prop="qualificationType" label="行业名称" />
+        <el-table-column prop="otherAsk" label="特殊资质是否必填">
+          <template #default="scope">
+            <span v-if="scope.row.otherAsk == 0">否</span>
+            <span v-if="scope.row.otherAsk == 1">是</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="askDetail" label="特殊资质" width="240" />
+      </el-table>
+    </el-dialog>
+    <!-- 优惠费率活动对照表 -->
+    <el-dialog v-model="dialogVisibleActivity" title="费率结算规则对照表" width="60%" draggable>
+    <el-table :data="activitytableData" height="580">
+      <el-table-column prop="activitiesId" label="活动费率ID" />
+      <el-table-column prop="activitiesName" label="活动名称" />
+      <el-table-column prop="baseRate" label="标准费率" />
+      <el-table-column prop="fitType" label="优惠区间">
+        <template #default="scope">
+          <span>{{ scope.row.minRate }} ~ {{ scope.row.maxRate }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="endTime" label="活动有效期" />
+      <el-table-column prop="activitiesDesc" label="活动介绍" />
+      <el-table-column prop="activitiesRule" label="活动规则" />
+    </el-table>
+  </el-dialog>
   </div>
 </template>
 
 <script setup>
 import {reactive, ref} from "vue";
 import LabelExplain from "@/views/insurance/customer/components/labelExplain";
-import {getToken} from "@/utils/auth";
-import {ElMessage} from "element-plus";
+import {listActivity, listSettlement} from "@/api/insurance/wechatIncoming";
+import ShpUploadFile from './ShpUploadFile.vue';
 
+const { proxy } = getCurrentInstance();
 const form = ref({
   settlementId: '', //入驻结算规则ID
   qualificationType: '', //所属行业
@@ -131,17 +136,33 @@ const form = ref({
   activitiesRate: '', //优惠费率活动值
   activitiesAdditions: [] //优惠费率活动补充材料
 })
-const addFileList = ref([]);
-const qFileList = ref([])
-const dialogVisible = ref(false);
-const dialogImageUrl = ref('');
-const qDialogVisible = ref(false);
-const qDialogImageUrl = ref('');
 
-const uploadData = ref({
-  uploadUrl: import.meta.env.VITE_APP_BASE_API + 'pay/media/wxPictureUpload',
-  token: getToken()
-})
+const dialogVisibleTable = ref(false)
+const tableData = ref([])
+
+const dialogVisibleActivity = ref(false)
+const activitytableData = ref([])
+
+const typeList = ref([])
+const otherAsk = ref(0)
+
+const onClick = (type) => {
+  if (type === 'settlement') {
+    dialogVisibleTable.value = true
+    listSettlement().then(res => {
+      if (res.code === 200) {
+        tableData.value = res.data
+      }
+    })
+  } else if (type === 'activity') {
+    dialogVisibleActivity.value = true
+    listActivity().then(res => {
+      if (res.code === 200) {
+        activitytableData.value = res.data
+      }
+    })
+  }
+}
 
 const rules = reactive({
   settlementId: [{ required: true, message: "请输入入驻结算规则ID", trigger: "blur" }],
@@ -149,49 +170,37 @@ const rules = reactive({
   qualifications: [{ required: true, message: "请上传特殊资质图片", trigger: "change" }],
   activitiesId: [{ required: true, message: "请输入优惠费率活动ID", trigger: "blur" }],
   activitiesRate: [{ required: true, message: "请输入优惠费率活动值", trigger: "blur" }],
-  activitiesAdditions: [{ required: true, message: "请上传优惠费率活动补充材料", trigger: "change" }],
 })
-// 上传验证
-const beforeAvatarUpload = (file) => {
-  const isPicture = file.type === 'image/jpeg' || file.type === 'image/png'
-  const isLt2M = file.size / 1024 / 1024 < 2
-  if (!isPicture) {
-    ElMessage.error('请上传jepg,png,jpg格式的图片!')
-    return false
-  } else if (!isLt2M) {
-    ElMessage.error('上传图片不能超过2M!')
-    return false
-  }
-  return true
-}
-//特殊资质图片
-const qHandleAvatarSuccess = (res) => {
-  if (res.code === 200) {
-    qFileList.value = form.value.qualifications
-  } else {
-    ElMessage.error(res.msg)
-    qFileList.value = []
-  }
-}
-const qHandlePictureCardPreview = (uploadFile) => {
-  qDialogImageUrl.value = uploadFile.url
-  qDialogVisible.value = true
+
+const handleChange = (val) => {
+  let obj = typeList.value.find(item => {
+    return item.id === val
+  })
+  form.value.qualificationType = obj.qualificationType
+  form.value.settlementId = obj.settlementId
+  otherAsk.value = obj.otherAsk
 }
 
-// 优惠费率活动补充材料
-const addHandleAvatarSuccess = (res) => {
-  if (res.code === 200) {
-    addFileList.value = form.value.activitiesAdditions
-  } else {
-    ElMessage.error(res.msg)
-    addFileList.value = []
-  }
-}
-const handlePictureCardPreview = (uploadFile) => {
-  dialogImageUrl.value = uploadFile.url
-  dialogVisible.value = true
+const emit = defineEmits(["result"]);
+const submit = () => {
+  proxy.$refs["settlementRef"].validate(valid => {
+    if (valid) {
+      emit('result', form.value)
+      console.log('结算规则' ,form.value)
+    } else {
+      emit('result', false)
+    }
+  })
 }
 
+const getList = () => {
+  listSettlement().then(res => {
+    if (res.code === 200) {
+      typeList.value = res.data
+    }
+  })
+}
+getList()
 </script>
 
 <style lang="scss" scoped>
