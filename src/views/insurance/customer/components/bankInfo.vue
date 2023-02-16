84cce15f451ae7699d30d3a55c37103ebaea0320
wangmeixi <wangmeixi@shanhaiping.com>
2023-02-16 09:20:00 +0800

    修改超级管理员信息，结算银行，结算规则

--- src/views/insurance/customer/components/bankInfo.vue
+++ src/views/insurance/customer/components/bankInfo.vue
@@ -14,7 +14,7 @@
           <template #label>
             <labelExplain label="账户类型">
               <template #explain>
-                <div>1、若主体为企业/政府机关/事业单位/社会组织，可填写：对公银行账户<br />2、若主体为个体户，可选择填写：对公银行账户或经营者个人银行卡</div>
+                <div>1、若主体为企业/政府机关/事业单位/社会组织，可填写：对公银行账户<br/>2、若主体为个体户，可选择填写：对公银行账户或经营者个人银行卡</div>
               </template>
             </labelExplain>
           </template>
@@ -32,23 +32,24 @@
           <template #label>
             <labelExplain label="开户名称">
               <template #explain>
-                <div>1、选择“经营者个人银行卡”时，开户名称必须与“经营者证件姓名”一致<br />2、选择“对公银行账户”时，开户名称必须与营业执照上的“商户名称”一致</div>
+                <div>1、选择“经营者个人银行卡”时，开户名称必须与“经营者证件姓名”一致<br/>2、选择“对公银行账户”时，开户名称必须与营业执照上的“商户名称”一致</div>
               </template>
             </labelExplain>
           </template>
-          <el-input v-model="form.accountName" placeholder="请输入开户名称" />
+          <el-input v-model="form.accountName" placeholder="请输入开户名称"/>
         </el-form-item>
 
         <el-form-item prop="accountBank">
           <template #label>
             <labelExplain label="开户银行">
               <template #explain>
-                <div>1、17家直连银行，请根据开户银行对照表直接填写银行名<br />2、非17家直连银行，该参数请填写为“其他银行”。</div>
+                <div>1、17家直连银行，请根据开户银行对照表直接填写银行名<br/>2、非17家直连银行，该参数请填写为“其他银行”。</div>
               </template>
             </labelExplain>
           </template>
           <el-select v-model="form.accountBank" placeholder="请选择开户银行" style="width: 100%;">
-            <el-option v-for="item in accountBankList" :key="item.dictValue" :label="item.dictLabel" :value="item.dictValue" />
+            <el-option v-for="item in accountBankList" :key="item.dictValue" :label="item.dictLabel"
+                       :value="item.dictValue"/>
           </el-select>
         </el-form-item>
 
@@ -60,14 +61,15 @@
               </template>
             </label-explain>
           </template>
-          <el-select v-model="state.province" @change="provincChange" placeholder="请选择省份" style="width: 32%;margin-right: 2%;">
-            <el-option v-for="item in provinceList" :value="item.id" :label="item.name" :key="item.id" />
+          <el-select v-model="state.province" @change="provincChange" placeholder="请选择省份"
+                     style="width: 32%;margin-right: 2%;">
+            <el-option v-for="item in provinceList" :value="item.id" :label="item.name" :key="item.id"/>
           </el-select>
           <el-select v-model="state.city" @change="cityChange" placeholder="请选择城市" style="width: 32%;margin-right: 2%;">
-            <el-option v-for="item in cityList" :value="item.id" :label="item.name" :key="item.id" />
+            <el-option v-for="item in cityList" :value="item.id" :label="item.name" :key="item.id"/>
           </el-select>
           <el-select v-model="state.area" @change="areaChange" placeholder="请选择地区" style="width: 32%;">
-            <el-option v-for="item in areaList" :value="item.id" :label="item.name" :key="item.id" />
+            <el-option v-for="item in areaList" :value="item.id" :label="item.name" :key="item.id"/>
           </el-select>
         </el-form-item>
 
@@ -79,26 +81,30 @@
               </template>
             </labelExplain>
           </template>
-          <el-input v-model="form.bankAddressCode" placeholder="请输入开户银行省市编码" />
+          <el-input v-model="form.bankAddressCode" placeholder="请输入开户银行省市编码"/>
         </el-form-item>
 
         <el-form-item prop="bankBranchId" v-if="form.accountBank === '其他银行'">
           <template #label>
             <labelExplain label="开户银行联行号">
               <template #explain>
-                <div>1、17家直连银行无需填写，如为其他银行，则开户银行全称（含支行）和开户银行联行号二选一，详细参见<el-link type="primary" @click="dialogVisible = true">开户银行全称（含支行）对照表</el-link></div>
+                <div>1、17家直连银行无需填写，如为其他银行，则开户银行全称（含支行）和开户银行联行号二选一，详细参见
+                  <el-link type="primary" @click="dialogVisible = true">开户银行全称（含支行）对照表</el-link>
+                </div>
               </template>
             </labelExplain>
           </template>
-          <el-input v-model="form.bankBranchId" placeholder="请输入银行联行号" />
+          <el-input v-model="form.bankBranchId" placeholder="请输入银行联行号"/>
         </el-form-item>
 
         <el-form-item prop="bankName" v-if="form.accountBank === '其他银行'">
           <template #label>
             <labelExplain label="开户银行全称（含支行）">
               <template #explain>
-                <div>1、17家直连银行无需填写，如为其他银行，则开户银行全称（含支行）和 开户银行联行号二选一<br />2、需填写银行全称，如“深圳农村商业银行XXX支行”,详细参见<el-link type="primary" @click="dialogVisible = true">开户银行全称（含支行）对照表</el-link></div>
-<!--                <div>1、17家直连银行无需填写，如为其他银行，则开户银行全称（含支行）和 开户银行联行号二选一<br />2、需填写银行全称，如“深圳农村商业银行XXX支行”</div>-->
+                <div>1、17家直连银行无需填写，如为其他银行，则开户银行全称（含支行）和 开户银行联行号二选一<br/>2、需填写银行全称，如“深圳农村商业银行XXX支行”,详细参见
+                  <el-link type="primary" @click="dialogVisible = true">开户银行全称（含支行）对照表</el-link>
+                </div>
+                <!--                <div>1、17家直连银行无需填写，如为其他银行，则开户银行全称（含支行）和 开户银行联行号二选一<br />2、需填写银行全称，如“深圳农村商业银行XXX支行”</div>-->
               </template>
             </labelExplain>
           </template>
@@ -129,19 +135,19 @@
               </template>
             </labelExplain>
           </template>
-          <el-input v-model="form.accountNumber" placeholder="请输入银行账号" />
+          <el-input v-model="form.accountNumber" placeholder="请输入银行账号"/>
         </el-form-item>
 
-<!--        <el-form-item>-->
-<!--          <el-button @click="submit">保存</el-button>-->
-<!--        </el-form-item>-->
+        <!--        <el-form-item>-->
+        <!--          <el-button @click="submit">保存</el-button>-->
+        <!--        </el-form-item>-->
       </el-form>
     </el-card>
     <el-dialog v-model="dialogVisible" title="开户银行全称（含支行）对照表" draggable>
       <el-input v-model="name" placeholder="请输入银行名称" @change="handleChange"></el-input>
       <el-table :data="tableData" v-loading="loading" height="580" style="margin-top: 20px;">
-        <el-table-column prop="bankBranchId" label="联行号" />
-        <el-table-column prop="bankName" label="开户银行全称" min-width="270" />
+        <el-table-column prop="bankBranchId" label="联行号"/>
+        <el-table-column prop="bankName" label="开户银行全称" min-width="270"/>
       </el-table>
     </el-dialog>
   </div>
@@ -153,7 +159,7 @@ import LabelExplain from "@/views/insurance/customer/components/labelExplain";
 import {bankInfo, bankType, listArea, listCity, listProvince} from "@/api/insurance/wechatIncoming";
 import {ElMessage} from "element-plus";
 
-const { proxy } = getCurrentInstance();
+const {proxy} = getCurrentInstance();
 const form = ref({
   bankAccountType: '',//账户类型
   accountName: '', //开户名称
@@ -181,13 +187,13 @@ const loading = ref(false)
 const tableData = ref([])
 
 const rules = reactive({
-  bankAccountType: [{ required: true, message: "请选择账户类型", trigger: "change" }],
-  accountName: [{ required: true, message: "请输入开户名称", trigger: "blur" }],
-  accountBank: [{ required: true, message: "请选择开户银行", trigger: "change" }],
-  bankAddressCode: [{ required: true, message: "请输入开户银行省市编码", trigger: "blur" }],
+  bankAccountType: [{required: true, message: "请选择账户类型", trigger: "change"}],
+  accountName: [{required: true, message: "请输入开户名称", trigger: "blur"}],
+  accountBank: [{required: true, message: "请选择开户银行", trigger: "change"}],
+  bankAddressCode: [{required: true, message: "请输入开户银行省市编码", trigger: "blur"}],
   // bankBranchId: [{ required: true, message: "请输入开户银行联行号", trigger: "blur" }],
   // bankName: [{ required: true, message: "请输入开户银行全称（含支行）", trigger: "blur" }],
-  accountNumber: [{ required: true, message: "请输入银行账号", trigger: "blur" }]
+  accountNumber: [{required: true, message: "请输入银行账号", trigger: "blur"}]
 })
 
 //省市区
@@ -248,8 +254,8 @@ const submit = () => {
       if (form.value.accountBank === '其他银行' && (form.value.bankBranchId == '' && form.value.bankName == '')) {
         ElMessage.error('请输入开户银行联行号或开户银行全称（含支行）')
       } else {
-        emit('result', form.value)
-        console.log('银行账户' ,form.value)
+        emit('result', {bankAccountInfo: form.value})
+        console.log('银行账户', form.value)
       }
     } else {
       emit('result', false)
@@ -278,21 +284,21 @@ defineExpose({
 
 <style lang="scss" scoped>
 .box-card {
-    width: 650px;
+  width: 650px;
 
-    .card-header {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      font-size: 24px;
-      font-weight: bold;
+  .card-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 24px;
+    font-weight: bold;
 
-      .desc {
-        width: 350px;
-        font-weight: normal;
-        font-size: 12px;
-        margin-left: 40px;
-      }
+    .desc {
+      width: 350px;
+      font-weight: normal;
+      font-size: 12px;
+      margin-left: 40px;
     }
+  }
 }
 </style>
