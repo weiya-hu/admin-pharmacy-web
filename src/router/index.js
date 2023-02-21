484fb55a55fa3a596df61dd4bef0a5aa0ecb0fcd
alices_x <1277787672@qq.com>
2023-02-21 17:34:54 +0800

    fix:公众号后台界面创建

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
-import { createWebHistory, createRouter } from 'vue-router'
-import Layout from '@/layout'
+import { createWebHistory, createRouter } from "vue-router";
+import Layout from "@/layout";
 
 /**
  * Note: 路由配置项
@@ -25,129 +25,148 @@ import Layout from '@/layout'
 // 公共路由
 export const constantRoutes = [
   {
-    path: '/redirect',
+    path: "/redirect",
     component: Layout,
     hidden: true,
     children: [
       {
-        path: '/redirect/:path(.*)',
-        component: () => import('@/views/redirect/index.vue')
+        path: "/redirect/:path(.*)",
+        component: () => import("@/views/redirect/index.vue")
       }
     ]
   },
   {
-    path: '/login',
-    component: () => import('@/views/login'),
+    path: "/login",
+    component: () => import("@/views/login"),
     hidden: true
   },
   {
-    path: '/register',
-    component: () => import('@/views/register'),
+    path: "/register",
+    component: () => import("@/views/register"),
     hidden: true
   },
   {
-    path: '',
+    path: "",
     component: Layout,
-    redirect: '/home',
+    redirect: "/home",
     children: [
       {
-        path: '/home',
-        component: () => import('@/views/index'),
-        name: 'Home',
-        meta: { title: '首页', icon: 'dashboard', affix: true }
-      }
-    ]
-  },
-
-  {
-    path: '/user',
-    component: Layout,
-    hidden: true,
-    redirect: 'noredirect',
-    children: [
-      {
-        path: 'profile',
-        component: () => import('@/views/system/user/profile/index'),
-        name: 'Profile',
-        meta: { title: '个人中心', icon: 'user' }
+        path: "/home",
+        component: () => import("@/views/index"),
+        name: "Home",
+        meta: { title: "首页", icon: "dashboard", affix: true }
       }
     ]
   },
   {
-    path: '/system/user-auth',
+    path: "/hospital",
     component: Layout,
-    hidden: true,
+    redirect: "/hospitalList",
+    meta: { title: "医院", icon: "dashboard" },
     children: [
       {
-        path: 'role/:userId(\\d+)',
-        component: () => import('@/views/system/user/authRole'),
-        name: 'AuthRole',
-        meta: { title: '分配角色', activeMenu: '/system/user' }
+        path: "hospitalList",
+        component: () => import("@/views/hospital/hospitalList"),
+        meta: { title: "医院列表", icon: "dashboard" },
+        name: "HospitalList"
+      },
+      {
+        path: "hospitalConfig",
+        component: () => import("@/views/hospital/hospitalConfig"),
+        name: "HospitalConfig",
+        hidden: true
       }
     ]
   },
   {
-    path: '/system/role-auth',
+    path: "/user",
     component: Layout,
     hidden: true,
+    redirect: "noredirect",
     children: [
       {
-        path: 'user/:roleId(\\d+)',
-        component: () => import('@/views/system/role/authUser'),
-        name: 'AuthUser',
-        meta: { title: '分配用户', activeMenu: '/system/role' }
+        path: "profile",
+        component: () => import("@/views/system/user/profile/index"),
+        name: "Profile",
+        meta: { title: "个人中心", icon: "user" }
       }
     ]
   },
   {
-    path: '/system/dict-data',
+    path: "/system/user-auth",
     component: Layout,
     hidden: true,
     children: [
       {
-        path: 'index/:dictId(\\d+)',
-        component: () => import('@/views/system/dict/data'),
-        name: 'Data',
-        meta: { title: '字典数据', activeMenu: '/system/dict' }
+        path: "role/:userId(\\d+)",
+        component: () => import("@/views/system/user/authRole"),
+        name: "AuthRole",
+        meta: { title: "分配角色", activeMenu: "/system/user" }
       }
     ]
   },
   {
-    path: '/monitor/job-log',
+    path: "/system/role-auth",
     component: Layout,
     hidden: true,
     children: [
       {
-        path: 'index',
-        component: () => import('@/views/monitor/job/log'),
-        name: 'JobLog',
-        meta: { title: '调度日志', activeMenu: '/monitor/job' }
+        path: "user/:roleId(\\d+)",
+        component: () => import("@/views/system/role/authUser"),
+        name: "AuthUser",
+        meta: { title: "分配用户", activeMenu: "/system/role" }
       }
     ]
   },
   {
-    path: '/tool/gen-edit',
+    path: "/system/dict-data",
     component: Layout,
     hidden: true,
     children: [
       {
-        path: 'index/:tableId(\\d+)',
-        component: () => import('@/views/tool/gen/editTable'),
-        name: 'GenEdit',
-        meta: { title: '修改生成配置', activeMenu: '/tool/gen' }
+        path: "index/:dictId(\\d+)",
+        component: () => import("@/views/system/dict/data"),
+        name: "Data",
+        meta: { title: "字典数据", activeMenu: "/system/dict" }
+      }
+    ]
+  },
+  {
+    path: "/monitor/job-log",
+    component: Layout,
+    hidden: true,
+    children: [
+      {
+        path: "index",
+        component: () => import("@/views/monitor/job/log"),
+        name: "JobLog",
+        meta: { title: "调度日志", activeMenu: "/monitor/job" }
+      }
+    ]
+  },
+  {
+    path: "/tool/gen-edit",
+    component: Layout,
+    hidden: true,
+    children: [
+      {
+        path: "index/:tableId(\\d+)",
+        component: () => import("@/views/tool/gen/editTable"),
+        name: "GenEdit",
+        meta: { title: "修改生成配置", activeMenu: "/tool/gen" }
       }
     ]
   },
   {
     path: "/:pathMatch(.*)*",
-    component: () => import('@/views/error/404'),
+    component: () => import("@/views/error/404"),
     hidden: true
   },
   {
-    path: '/401',
-    component: () => import('@/views/error/401'),
+    path: "/401",
+    component: () => import("@/views/error/401"),
     hidden: true
-  },
+  }
 ];
 
 const router = createRouter({
@@ -155,11 +174,11 @@ const router = createRouter({
   routes: constantRoutes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
-      return savedPosition
+      return savedPosition;
     } else {
-      return { top: 0 }
+      return { top: 0 };
     }
-  },
+  }
 });
 
 export default router;
